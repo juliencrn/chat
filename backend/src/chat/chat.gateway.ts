3ef6be33ca792cfd5d14a1c224fce5cc0a0484ea
Julien <juliencaron@protonmail.com>
2021-10-07 01:34:35 +0200

    [Backend] Make socket.io working

---
+++ backend/src/chat/chat.gateway.ts
@@ -0,0 +1,56 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  MessageBody,
+  OnGatewayInit,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+import { randomUUID } from 'crypto';
+
+@WebSocketGateway({
+  cors: { origin: 'http://localhost:3000' },
+})
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  userCount = 0;
+
+  private logger: Logger = new Logger('ChatGateway');
+
+  async afterInit(/*server: Server*/) {
+    this.logger.log('Init');
+  }
+
+  async handleConnection(client: Socket) {
+    this.logger.log(`Client connected ${client.id}`);
+    this.userCount++;
+    this.server.emit('user_count', this.userCount);
+  }
+
+  async handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected ${client.id}`);
+    this.userCount--;
+    this.server.emit('user_count', this.userCount);
+  }
+
+  @SubscribeMessage('message')
+  async onChat(@MessageBody() [username, text]: [string, string]) {
+    // TODO: Replace by a Model
+    const chatMessage = {
+      id: randomUUID(),
+      postedAt: Date.now(),
+      username,
+      text,
+    };
+
+    this.logger.log(`Message received "${text}"`, chatMessage);
+    this.server.emit('message', chatMessage);
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:3000' },
+  });
+
+  await app.listen(5000);
 }
 bootstrap();
