809caba1fe5b19432e9176253252aef788c3ad4e
Julien <juliencaron@protonmail.com>
2021-10-10 02:14:19 +0200

    [frontend] Implement react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,75 @@
 import React from 'react';
 import { useSelector } from "react-redux";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  RouteProps
+} from "react-router-dom";
 
 import Chat from './screens/Chat';
 import Login from './screens/Login';
 import { RootState } from './state/store';
 
 function App() {
-  const { user } = useSelector((state: RootState) => state.auth)
+  return <AppRouter />
+}
 
-  // TODO: Use a router instead
-  if (!user) {
-    return <Login />
-  } 
-
-  return <Chat />
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <PublicOnlyRoute path="/login">
+          <Login />
+        </PublicOnlyRoute>
+        <PrivateRoute path="/">
+          <Chat />
+        </PrivateRoute>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+function useAuth(): boolean {
+  const auth = useSelector((state: RootState) => state.auth)
+  return !!auth.user && !!auth.accessToken
+}
+
+function PrivateRoute({ children, ...props }: RouteProps) {
+  const isAuth = useAuth()
+  return (
+    <Route 
+      {...props}
+      render={({ location }) => isAuth
+        ? children
+        : (
+          <Redirect to={{
+            pathname: "/login",
+            state: { from: location }
+          }} />
+        )
+      }
+    />
+  )
+}
+
+function PublicOnlyRoute({ children, ...props }: RouteProps) {
+  const isAuth = useAuth()
+  return (
+    <Route 
+      {...props}
+      render={({ location }) => !isAuth
+        ? children
+        : (
+          <Redirect to={{
+            pathname: "/",
+            state: { from: location }
+          }} />
+        )
+      }
+    />
+  )
+}
