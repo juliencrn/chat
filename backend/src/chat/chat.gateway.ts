d85898260eca56f9800adce32cf5759718f393b2
Julien <juliencaron@protonmail.com>
2021-10-11 01:32:05 +0200

    [Backend] Update websocket gateway

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -20,15 +20,16 @@ import MongooseClassSerializerInterceptor from 'src/interceptors/mongooseClassSe
 import { User } from 'src/users/schemas/user.schema';
 import { UsersService } from 'src/users/users.service';
 
-enum EventNames {
-  user_count = 'user_count',
-  message = 'message',
+interface UserConnection {
+  userId: string;
+  connectionId: string;
 }
 
 @UseGuards(WebSocketJwtAuthGuard)
 @WebSocketGateway({
   // TODO: Use environnement variable instead
   cors: { origin: 'http://localhost:3000' },
+  transports: ['websocket'],
 })
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
@@ -36,8 +37,8 @@ export class ChatGateway
   @WebSocketServer()
   server: Server;
 
-  userCount = 0;
-
+  private userCount = 0;
+  private users = new Map();
   private logger: Logger = new Logger('ChatGateway');
 
   constructor(
@@ -50,35 +51,65 @@ export class ChatGateway
   }
 
   async handleConnection(client: Socket) {
-    this.logger.log(`Client connected ${client.id}`);
-    // console.log('handshake', client.handshake);
-    this.userCount++;
-    this.server.emit(EventNames.user_count, this.userCount);
+    const userMap = this.getUserMap(client);
+    const userConnection = this.mapToUserConnection(userMap);
+
+    // Print new connected user
+    this.logger.log(`Client connected`, userConnection);
+    this.server.emit('user', userConnection);
+
+    // Add user to the users map
+    this.users.set(...userMap);
+
+    // Print users map
+    const users = [...this.users].map(this.mapToUserConnection);
+    this.server.emit('users', users);
   }
 
   async handleDisconnect(client: Socket) {
-    this.logger.log(`Client disconnected ${client.id}`);
-    this.userCount--;
-    this.server.emit(EventNames.user_count, this.userCount);
+    const userMap = this.getUserMap(client);
+    const userConnection = this.mapToUserConnection(userMap);
+
+    // Print new disconnected user
+    this.logger.log(`Client disconnected`, userConnection);
+    this.server.emit('user', userConnection);
+
+    // Remove user from the users map
+    this.users.delete(userMap[0]);
+
+    // Print users map
+    const users = [...this.users].map(this.mapToUserConnection);
+    this.server.emit('users', users);
   }
 
-  @SubscribeMessage(EventNames.message)
+  @SubscribeMessage('message')
   async onChat(@MessageBody() [userId, text]: [string, string]) {
     const chatMessage = await this.messagesService.create({ userId, text });
     const serializedMessage = this.messagesService.serialize(chatMessage);
 
     this.logger.log(`Message received "${text}"`);
-    this.server.emit(EventNames.message, excludePrefix(serializedMessage));
+    this.server.emit('message', excludePrefix(serializedMessage));
+  }
+
+  private mapToUserConnection(
+    userMapItem: [userId: string, connectionId: string],
+  ): UserConnection {
+    return {
+      userId: userMapItem[0],
+      connectionId: userMapItem[1],
+    };
+  }
+
+  private getUserMap(client: Socket): [string, string] {
+    const userId: string = client.handshake.query.userId.toString();
+    return [userId, client.id];
   }
 }
 
 // TODO: It's not the Nest way
-function excludePrefix(
-  obj: Record<string, any>,
-  prefix?: string,
-): Record<string, any> {
+function excludePrefix(obj: Record<string, any>): Record<string, any> {
   for (const key in obj) {
-    if (key.slice(0, 1) === prefix ?? '_') {
+    if (key.slice(0, 1) === '_') {
       delete obj[key];
     }
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
