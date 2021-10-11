3540ba16bbc408d98eb6d0a758f5db7775316f69
Julien <juliencaron@protonmail.com>
2021-10-11 02:44:15 +0200

    [frontend] Add prettier, eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: ['react', 'simple-import-sort', 'prettier', 'jsx-a11y'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: '17.0.2',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/prop-types': 'off',
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'simple-import-sort/exports': 'warn',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          ['^\\u0000'], // side effect (E.g.'import 'normalize.css'')
+          ['^react$'],
+          ['^[^.]'], // Libs
+          ['^../|^~/|^./'],
+        ],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+          },
+        ],
+      },
+    },
+    // {
+    //   extends: ['plugin:jest/recommended'],
+    //   files: ['**/*.test.ts'],
+    //   plugins: ['jest'],
+    //   env: {
+    //     jest: true,
+    //   },
+    //   rules: {
+    //     // you should turn the original rule off *only* for test files
+    //     '@typescript-eslint/unbound-method': 'off',
+    //     'jest/unbound-method': 'error',
+    //   },
+    // },
+  ],
+}
