dbc7648c464f7b4b20afb3e57342cbb19a9fea3c
Julien <juliencaron@protonmail.com>
2021-10-15 05:01:38 +0200

    [frontend] Add tailwind in bundle

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./state/store";
