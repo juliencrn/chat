7758e53c000537f0a81fa8dfac9723ac6ecf648f
Julien <juliencaron@protonmail.com>
2021-10-22 04:58:14 +0200

    [Backend] Improve mongo connect allowing remote db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
-import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule, MongooseModuleOptions } from "@nestjs/mongoose";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -11,14 +11,21 @@ import { MessagesModule } from "./messages/messages.module";
 import { ThreadsModule } from "./threads/threads.module";
 import { UsersModule } from "./users/users.module";
 
+// TODO: Use "only" environnement variable instead, throw instead
+const mongo_uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/chat";
+const mongooseOptions: MongooseModuleOptions = {
+  retryWrites: true,
+  w: "majority",
+  ssl: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 @Module({
   imports: [
     ChatModule,
     MessagesModule,
-    // TODO: Use environnement variable instead
-    MongooseModule.forRoot(
-      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/chat",
-    ),
+    MongooseModule.forRoot(mongo_uri, mongooseOptions),
     AuthModule,
     UsersModule,
     ThreadsModule,
